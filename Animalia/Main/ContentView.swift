d9d7727c6a1b73074b94e14721a9e6d29bb70483
Dicky-Fahriza <dickyfahriza46@gmail.com>
2024-04-29 10:39:17 +0700

    Add cover image and animal list to ContentView

--- Animalia/Main/ContentView.swift
+++ Animalia/Main/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    let animal: [AnimalModel] = Bundle.main.decode("animals.json")
+    
     var body: some View {
         NavigationStack {
-            CoverImageView()
-                .frame(height: 300)
+            Group {
+                List {
+                    CoverImageView()
+                        .frame(height: 300)
+                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    
+                    ForEach(animal) { animal in
+                        AnimalListItemView(animal: animal)
+                    }
+                    
+                    
+                }
+                .listStyle(.plain)
+            }
+            .navigationTitle("Animalia")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 16) {
+                        // List
+                        Button {
+                            //: TODO isGridViewActive
+                        } label: {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundStyle(.accent)
+                        }
+                        
+                        // GRID
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundStyle(.accent)
+                        }
+                        
+                    }
+                }
+            }
         }
-        .navigationTitle("Animalia")
     }
 }
 
